b2fd99f5452d34fb9365e98cd81de5644332b8cd
Fabien Benoit-Koch <fabien.benoit@gmail.com>
2023-10-01 18:46:51 +0200

    Tutorial

---
+++ Tutorial.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public class Tutorial : Control
+{
+	private GameProgressManager _gameProgressManager;
+	
+    public override void _Ready()
+    {
+		_gameProgressManager = (GameProgressManager)GetNode($"/root/{nameof(GameProgressManager)}");
+
+		if (_gameProgressManager.CurrentLevel != 0)
+		{
+			this.Visible = false;
+		}
+    }
+    
+    public void OnClick()
+    {
+	    GD.Print("Close tutorial");
+	    this.Visible = false;
+    }
+    
+}
