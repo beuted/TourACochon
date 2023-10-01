8b23b59ef01ff2ec4c83c38f94ef6d4931aa7b42
Benoit Dekajoo <dekajoo@gmail.com>
2023-10-01 13:17:57 +0200

    Smol fix

--- UI/ResetLevelButton.cs
+++ UI/ResetLevelButton.cs
@@ -5,14 +5,18 @@ public class ResetLevelButton : TextureButton
 {
     private GameProgressManager _gameProgressManager;
 
-
     private TextureRect _textureRect;
-
+    private TextureRect _textureRectStartStopButton;
+    private Texture _startButtonTexture;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         _textureRect = GetNode<TextureRect>("TextureRect");
+
+        _textureRectStartStopButton = GetNode<TextureRect>("../StartStopButton/TextureRect");
+
+        _startButtonTexture = ResourceLoader.Load<Texture>("res://assets/graphics/start-button.png");
 
         _gameProgressManager = (GameProgressManager)GetNode($"/root/{nameof(GameProgressManager)}"); // Singleton
     }
@@ -20,6 +24,9 @@ public class ResetLevelButton : TextureButton
     public void OnClick()
     {
         _gameProgressManager.ResetLevel();
+
+        // Very hacky mais la flemme
+        _textureRectStartStopButton.Texture = _startButtonTexture;
     }
 
 }
