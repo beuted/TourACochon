2d64971141aae0107602a409f69c02c868861cb2
Fabien Benoit-Koch <fabien.benoit@gmail.com>
2023-10-01 14:15:31 +0200

    Multiple animated sprite (+ correct mapping to perks)

--- entities/Item.cs
+++ entities/Item.cs
@@ -4,9 +4,6 @@ using System;
 public class Item : Node2D
 {
     public PigPerks Perks = PigPerks.None;
-
-    private AnimatedSprite _sprite;
-
     public Vector2 Destination;
     public Vector2 Direction = Vector2.Zero;
 
@@ -14,12 +11,23 @@ public class Item : Node2D
 
     public override void _Ready()
     {
-        _sprite = GetNode<AnimatedSprite>("AnimatedSprite");
-
-        _sprite.Frame = (int)Perks;
-
+        switch (Perks)
+        {
+            case PigPerks.None:
+                GetNode<AnimatedSprite>("Cochon_Dirty").Visible = true;
+                break;
+            case PigPerks.Cleaned:
+                GetNode<AnimatedSprite>("Cochon_Clean").Visible = true;
+                break;
+            case PigPerks.Fat:
+                GetNode<AnimatedSprite>("Cochon_Fat").Visible = true;
+                break;
+            default:
+                GetNode<AnimatedSprite>("Cochon_Clean").Visible = true;
+                break;
+        }
         Destination = Position;
-    }
+    }   
 
     public override void _Process(float delta)
     {
--- models/ItemType.cs
+++ models/ItemType.cs
@@ -4,7 +4,7 @@ public enum PigPerks
 {
   None = 0,
   Cleaned = 1,
-  StomachFull = 1 << 1,
+  Fat = 1 << 1,
   Vaccinated = 1 << 2,
   Strong = 1 << 3,
   Happy = 1 << 4,
